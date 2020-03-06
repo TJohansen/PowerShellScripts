1c325e51f6582ced9b87d0b4eddec17ab56f6f33
Thomas Johansen <t@tjohansen.net>
2020-03-06 11:22:24 +0100

    Update ADUsersLastLogonDate.ps1
    Added admin requirement and export to current working folder. Cleaned up.

--- ADUsersLastLogonDate.ps1
+++ ADUsersLastLogonDate.ps1
@@ -1,23 +1,27 @@
-﻿import-module ActiveDirectory 
- 
+#Script must be run as Admin or the lastlogondate property will be blanked out
+#Requires -RunAsAdministrator
+
+import-module ActiveDirectory 
+
 #Set the domain to search at the Server parameter. Run powershell as a user with privilieges in that domain to pass different credentials to the command. 
 #Searchbase is the OU you want to search. By default the command will also search all subOU's. To change this behaviour, change the searchscope parameter. Possible values: Base, onelevel, subtree 
 #Ignore the filter and properties parameters 
- 
+
 $ADUserParams=@{ 
 'Server' = '*' 
 'Searchbase' = '*'
 'Searchscope'= 'subtree' 
 'Filter' = 'enabled -eq $true'
-'Properties' = 'lastLogonDate' 
+'Properties' = '*' 
 } 
- 
-#This is where to change if different properties are required. 
+
+#Change if different properties are required. 
  
 $SelectParams=@{ 
-'Property' = 'employeeID', 'SAMAccountname','Userprincipalname', 'mobilephone', 'EmailAddress', 'lastlogondate'
+'Property' = 'SAMAccountname', 'mobilephone', 'EmailAddress', 'lastlogondate'
 } 
 
 # 'Property' = 'SAMAccountname','Userprincipalname', 'CN', 'title', 'DisplayName', 'Description', 'EmailAddress', 'mobilephone', 'office', 'officephone', 'state', 'streetaddress', 'city', 'employeeID', 'Employeenumber', 'enabled', 'lockedout', 'lastlogondate', 'badpwdcount', 'passwordlastset', 'created' 
- 
-get-aduser @ADUserParams | select-object @SelectParams | export-csv -delimiter ";" -Encoding UTF8 "c:\service\ADUsersLastLogonDate.csv"
+#Export-csv exports the output to the current working folder
+
+get-aduser @ADUserParams | select-object @SelectParams | Sort-Object -Property lastlogondate | export-csv -delimiter ";" -Encoding UTF8 ".\ADUsersLastLogonDate.csv" -NoTypeInformation
